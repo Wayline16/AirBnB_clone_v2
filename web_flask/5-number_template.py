3305a9a9d4bf7f67795156dfad3eba14dea4d7a6
Wayline Jeffries <jeffrieswayline@gmail.com>
2024-06-11 12:22:45 +0200

    pycodestyle

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Starts a flask web app
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -11,6 +12,7 @@ def index():
     returns Hello HBNB!
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -45,6 +47,7 @@ def imanumber(n):
 def displaynumber(n):
     """display HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
